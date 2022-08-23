8e3828fff9ecef58ad68a21a0f65c3736d131544
dwaynediddy <burrows.dwayne@icloud.com>
2022-08-23 16:41:59 +0200

    setting up for deployment

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -8,12 +8,12 @@ function Banner() {
         <div className="px-10 space-y-5">
             <h1 className='text-6xl max-w-xl font-serif'>
                 <span className='underline decoration-white decoration-4'>
-                    Diddy's
+                    Diddys
                 </span>
                 {" "}
                 Blogs are going to be Legen, wait for it... dary! LEGENDARY
              </h1>
-            <h2>Inspired by Barney 'The Barnicle' Stinsons blogs from HIMYM</h2>
+            <h2>Inspired by Barney The Barnicle Stinsons blogs from HIMYM</h2>
         </div>
         <div className='hidden md:inline-flex h-32 lg:h-full'>
             <Image src={Barney} height={500} width={500} alt='' />
